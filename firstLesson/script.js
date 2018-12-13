9c4974d0608bc8dee43606bf56d2c49753d1b3f4
Heilagr <weblow.ru@gmail.com>
2018-12-13 18:35:56 +0300

    Added validation

--- firstLesson/script.js
+++ firstLesson/script.js
@@ -1,20 +1,36 @@
-var budget = +prompt("Enter your budget");
-var shopName = prompt("Enter your shop name");
+let money = +prompt("Enter your budget");
+let name = prompt("Enter your shop name");
+let time = 19;
 
-var arr = ['meat', 'vegetables', 'books'];
-
-var mainList = {
-	budget: budget,
-	shopName: shopName,
-	shopGoods: arr,
-	employers: 2,
-	open: true
+let mainList = {
+	budget: money,
+	shopName: name,
+	shopGoods: [],
+	employers: {},
+	open: false
 }
 
-for (var i = 0; i < 3; i++) {
-	var ask = prompt("What kind of goods will you sell?");
-	arr.push(ask);
+for (let i = 0; i < 3; i++) {
+	let ask = prompt("What kind of goods will you sell?");
+
+	if((typeof(ask)) === 'string' && (typeof(ask)) === null && ask != '' && ask.length < 50) {
+		console.log('That\'s right!');
+		mainList.shopGoods[i] = ask;
+	} else {
+		i = i - 1;
+	}
+}
+
+if(time < 0) {
+	console.log('That\'s impossible!');
+} else if(time > 8 && time < 20) {
+	console.log('It time to work!');
+} else if(time < 24) {
+	console.log('It too late!');
+} else {
+	console.log('There is only 24 hours in a day!');
 }
 
 alert(mainList.budget / 30);
-console.log(mainList.shopGoods);
+
+console.log(mainList);
