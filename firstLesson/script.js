626e39b67a96b6c78d079075d0675fee6043e9c1
Heilagr <weblow.ru@gmail.com>
2018-12-18 01:39:41 +0300

    Let's try something else

--- firstLesson/script.js
+++ firstLesson/script.js
@@ -3,9 +3,9 @@ let money,
 	time,
 	price 
 
-function start(argument) {
-	money = +prompt("Enter your budget");
-	name = prompt("Enter your shop name").toUpperCase();
+function start() {
+	money = +prompt("Enter your budget", "");
+	name = prompt("Enter your shop name", "").toUpperCase();
 	time = 19;
 }
 
@@ -17,54 +17,52 @@ let mainList = {
 	shopGoods: [],
 	employers: {},
 	open: false,
-	discount: false
-}
+	discount: false,
+	shopItems: [],
+	chooseGoods: function chooseGoods() {
+		for (let i = 0; i < 3; i++) {
+			let ask = prompt("What kind of goods will you sell?", "");
 
-function chooseGoods() {
-	for (let i = 0; i < 3; i++) {
-		let ask = prompt("What kind of goods will you sell?");
-
-		if((typeof(ask)) === 'string' && (typeof(ask)) === null && ask != '' && ask.length < 50) {
-			console.log('That\'s right!');
-			mainList.shopGoods[i] = ask;
-		} else {
-			i = i - 1;
+			if((typeof(ask)) === 'string' && (typeof(ask)) === null && ask != '' && ask.length < 50) {
+				console.log('That\'s right!');
+				mainList.shopGoods[i] = ask;
+			} else {
+				i = i - 1;
+			}
 		}
+	},
+	workTime: function workTime(time) {
+		if(time < 0) {
+			console.log('That\'s impossible!');
+		} else if(time > 8 && time < 20) {
+			console.log('It time to work!');
+			mainList.open = true;
+		} else if(time < 24) {
+			console.log('It too late!');
+		} else {
+			console.log('There is only 24 hours in a day!');
+		}
+	},
+	budgetCount: function budgetCount() {
+		alert(mainList.budget / 30);	
+	},
+	makeDiscount: function makeDiscount() {
+		if (mainList.discount == true) {
+			price = (price / 100) * 80;
+		}
+	},
+	hireEmployers: function hireEmployers() {
+		for (let i = 0; i < 4; i++) {
+			let name = prompt("Enter employer name!", "");
+			mainList.employers[i] = name;
+		}
+	},
+	chooseShopItem: function chooseShopItem() {
+		let items = prompt("Enter your items separated with comma!", "")
+		mainList.shopItems = items.split(",");
+		mainList.shopItems.push(prompt("Wait, something else?", ""));
+		mainList.shopItems.sort();
 	}
 }
-chooseGoods();
-
-function workTime(time) {
-	if(time < 0) {
-		console.log('That\'s impossible!');
-	} else if(time > 8 && time < 20) {
-		console.log('It time to work!');
-	} else if(time < 24) {
-		console.log('It too late!');
-	} else {
-		console.log('There is only 24 hours in a day!');
-	}
-}
-workTime();
-
-function budgetCount() {
-	alert(mainList.budget / 30);	
-}
-budgetCount();
-
-function discountSystem() {
-	if (mainList.discount == true) {
-		price = price * 0.80;
-	}
-}
-discountSystem();
-
-function hireEmployers() {
-	for (var i = 0; i < 4; i++) {
-		employerName = prompt("Enter employer name!");
-		mainList.employers[i] = employerName;
-	}
-}
-hireEmployers();
 
 console.log(mainList);
